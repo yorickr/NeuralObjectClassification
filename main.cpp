9da131b9a64f3f1480719deddc665ba86af622f1
Remco Vorthoren <rvorthoren@outlook.com>
2017-12-18 14:17:18 +0100

    add new plaatjes

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 #include "./include/includes.h"
 
+#include "./include/trainingset.h"
+
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -13,37 +15,39 @@ int main(int argc, char** argv) {
 	for (int i = 1; i < argc; i++) {
 		cout << argv[i] << " ";
 	}
-	cout << endl;
-	size_t size_input = 2;
-	size_t size_hidden = 5;
-	size_t size_output = 1;
+	//cout << endl;
+	//size_t size_input = 2;
+	//size_t size_hidden = 5;
+	//size_t size_output = 1;
 
-	// 0 0 		0
-	// 0 1 		0
-	// 1 0 		0
-	// 1 1 		1
-	Matrix input(4, size_input);
-	input.mat[1][1] = 1;
-	input.mat[2][0] = 1;
-	input.mat[3][0] = 1;
-	input.mat[3][1] = 1;
-	cout << endl << endl;
-	cout << "Training input" << endl << input << endl;
+	//// 0 0 		0
+	//// 0 1 		0
+	//// 1 0 		0
+	//// 1 1 		1
+	//Matrix input(4, size_input);
+	//input.mat[1][1] = 1;
+	//input.mat[2][0] = 1;
+	//input.mat[3][0] = 1;
+	//input.mat[3][1] = 1;
+	//cout << endl << endl;
+	//cout << "Training input" << endl << input << endl;
 
-	Matrix output(4, size_output);
-	output.mat[3][0] = 1;
-	cout << "Training expected output" << endl << output << endl;
+	//Matrix output(4, size_output);
+	//output.mat[3][0] = 1;
+	//cout << "Training expected output" << endl << output << endl;
 
-	BPN bpn(input, output, size_hidden);
-	// cout << bpn << endl;
+	//BPN bpn(input, output, size_hidden);
+	//// cout << bpn << endl;
 
-	bpn.train(10000);
-	// cout << bpn << endl;
+	//bpn.train(10000);
+	//// cout << bpn << endl;
 
-	for (size_t row = 0; row < input.rows; row++) {
-		Matrix r(input[row], true); // transpose
-		bpn.guess(r);
-	}
+	//for (size_t row = 0; row < input.rows; row++) {
+	//	Matrix r(input[row], true); // transpose
+	//	bpn.guess(r);
+	//}
+
+	TrainingSet testSet1("C:/School/training_plaatjes/adapter");
 
 	return 0;
 }
